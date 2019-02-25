71845a9b043c89055dcb4b1b1dddb3cac99efb41
Przemyslaw Ciurzynski <przemyslaw.ciurzynski@exorigo-upos.pl>
2019-02-25 13:10:28 +0100

    Bundle configuration

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,13 +18,13 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
+
         $rootNode = $treeBuilder->root('iron_code_aws');
 
         $rootNode
             ->children()
-                ->scalarNode('region')->end()
-            ->end()
-        ;
+                ->variableNode('region')
+            ->end();
 
         return $treeBuilder;
     }
